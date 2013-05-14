ed3b575b13a497a7d146a767e1380d86542bf60b
Yuriy Zubarev <yuriyzubarev7@gmail.com>
2013-05-14 01:22:08 -0400

    library def

--- project.clj
+++ project.clj
@@ -2,7 +2,9 @@
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
   :dependencies [[org.clojure/clojure "1.5.1"]
-                 [compojure "1.1.5"]]
+                 [compojure "1.1.5"]
+                 [ring/ring-core "0.8.3"]
+    	         [ring/ring-jetty-adapter "0.8.3"]]
   :plugins [[lein-ring "0.8.3"]]
   :ring {:handler wad.handler/app}
   :profiles
