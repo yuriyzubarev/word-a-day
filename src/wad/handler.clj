7ad39946101563ef8540b39c9a52c875afe7962e
Yuriy Zubarev <yuriyzubarev7@gmail.com>
2013-05-14 01:11:51 -0400

    config

--- src/wad/handler.clj
+++ src/wad/handler.clj
@@ -1,7 +1,8 @@
 (ns wad.handler
   (:use compojure.core)
   (:require [compojure.handler :as handler]
-            [compojure.route :as route]))
+            [compojure.route :as route]
+            [ring.adapter.jetty :as ring]))
 
 (defroutes app-routes
   (GET "/" [] "Hello World")
@@ -10,3 +11,19 @@
 
 (def app
   (handler/site app-routes))
+
+(defn start [port]
+  (ring/run-jetty (var app)
+    	  {:port (or port 8080) :join? false}))
+
+(defn -main
+  ([] (-main 8080))
+  ([port]
+     (let [sys-port (System/getenv "PORT")]
+       (if (nil? sys-port)
+	 (start (cond
+		 (string? port) (Integer/parseInt port)
+		 :else port))
+	 (start (Integer/parseInt sys-port))))))
+
+
